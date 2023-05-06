519fe39377c3917839ef29d21689d0bd902f9bcf
aniketjha1729 <kumaraniket1729@gmail.com>
2023-05-06 07:06:41 +0530

    bootstrap setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="row">
+        <div className="col-6 text-jc-text">hello</div>
+        <div className="col-6 text-jc-red">hello</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
